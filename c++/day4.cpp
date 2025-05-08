d079bb23c48243c8b3129956b2ad8cf79d357b60
Markivanarcega01 <arcegamarkivan@gmail.com>
2025-05-09 01:07:53 +0800

    Day 4 Arithmetic

---
+++ c++/day4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int num1 = 10;
+    int num2 = 20;
+    int num3 = 30;
+
+    cout << num1 + num2 << endl;
+    cout << num2 - num3 << endl;
+    cout << num3 * num1 << endl;
+    cout << num1 / num2 << endl;
+    cout << num2 % num3 << endl; 
+
+    return 0;
+}
