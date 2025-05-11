f8b8b11756e5a4c5cef2cd49ed69eafb40933d82
Mark Ivan Arcega <Arcegamarkivan@gmail.com>
2025-05-11 19:48:13 +0800

    Day 7 and 8: Comments and If Else

---
+++ c++/day7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //Comments in C++
+    //Single comment
+    /* Multi-line
+    comment
+    I can type anything here
+    */
+    return 0;
+}
---
+++ c++/day8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    bool isTrue = true;
+
+    if(isTrue){
+        cout << "Output is " << isTrue;
+    }else{
+        cout << "Output is " << isTrue;
+    }
+
+    return 0;
+}
