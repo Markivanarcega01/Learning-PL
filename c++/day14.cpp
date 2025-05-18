e665ca3001751663dcd279f953e4adf382b10631
Mark Ivan Arcega <Arcegamarkivan@gmail.com>
2025-05-18 17:36:32 +0800

    Day 14:  Program using all operators

--- c++/day14.cpp
+++ c++/day14.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 int main(){
+    int num1 = 0;
+    int num2 = 0;
+
+    if(!num1 || !num2){
+        cout << "numbers are missing" << endl;
+    }else if(num1 % 2 == 0 && num2 % 2 == 0){
+        cout << "Both numbers are even";
+    }
 
     return 0; 
 }
