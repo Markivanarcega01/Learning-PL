e89f11b8a2efe569caa2302bcdc6e5f6bf45ab56
Markivanarcega01 <arcegamarkivan@gmail.com>
2025-05-20 15:16:33 +0800

    day3: Data types in C#

---
+++ c#/day3.cs
@@ -0,0 +1,9 @@
+using System;
+
+int num = 10;
+string name = "jhin";
+bool isRegistered = true;
+double weight = 22.5;
+char middleInitial = 'b';
+
+Console.WriteLine($"Hello My name is {name} and I am {num}y/o, \n{isRegistered}:{weight}:{middleInitial}");
