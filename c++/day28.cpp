e6fa40458eee87df4e658caf4c41efbac089422b
Markivanarcega01 <arcegamarkivan@gmail.com>
2025-06-26 11:09:17 +0800

    Day 28: In-depth understanding of const

--- c++/day28.cpp
+++ c++/day28.cpp
@@ -2,6 +2,27 @@
 
 int main(){
 
+    const int num = 2; // Cannot change the value
+    const int *num2 = new int;
+    //same with int const *num2 = new int;
+    /**
+     * *num2 = 2; Changing the actual value is forbidden
+        num2 = (int*) &num; But changing the address is possible.
+     *  
+     * */ 
+    
+    int* const num3 = new int;
+
+    /*
+    *num3 = 3; Change the actual value is possible
+    num3 = (int*) &num; Changeing the address is forbidden
+    */
+
+    const int* const num4 = new int;
+    /*
+        *num4 and num4 is both forbidden to change.
+    */
+
     const double pi = 3.14;
     double gravity = 9.8;
 
